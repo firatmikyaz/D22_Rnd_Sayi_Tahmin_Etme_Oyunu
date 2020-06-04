823a01786072e184d851b092bb08d534799d3ced
firatmikyaz <66138781+firatmikyaz@users.noreply.github.com>
2020-06-04 16:23:15 +0300

    Add project files.

---
+++ D22_Rnd_Sayi_Tahmin_Etme_Oyunu/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D22_Rnd_Sayi_Tahmin_Etme_Oyunu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sayı Tahmin Etme Oyunu //
+            
+            Random rnd = new Random();
+            int rastgele = rnd.Next(10, 20);
+            Console.WriteLine("Üretilen Sayı:{0}", rastgele);
+            int hak = 3;
+            BASADON:
+            Console.Write("Bir sayı tahmininde bulununuz?");
+            int tahmin = Convert.ToInt32(Console.ReadLine());
+            if (tahmin == rastgele)
+            {
+                Console.Write("Tebrikler... Puan:{0}", hak * 10);
+            }
+            else if (tahmin < rastgele)
+            {
+                hak--;
+                if (hak == 0)
+                {
+                    Console.Write("Hakkınız bitmiştir.");
+                }
+                else
+                {
+                    Console.Write("Tahmini büyütünüz!");
+                    goto BASADON;
+                }
+            }
+            else if (tahmin > rastgele)
+            {
+                hak--;
+                if(hak == 0) 
+                {
+                    Console.Write("Hakkınız bitmiştir.");
+                }
+                else
+                {
+                    Console.Write("Tahmini küçültünüz.");
+                    goto BASADON;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
